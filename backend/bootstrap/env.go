fcce77f393ccacd37661eef5f8fd1de164bbf78d
Braxton Hardman <braxtonhardman@gmail.com>
2025-10-03 14:17:25 -0700

    Env reading update

--- backend/bootstrap/env.go
+++ backend/bootstrap/env.go
@@ -33,12 +33,20 @@ func NewEnv() *Env {
 
 	// Read in the config file from memory searching in the defined path which we did above using SetConfigFile
 	viper.AutomaticEnv()          // override with environment variables
-
-	if env.AppEnv == "development" {
-		log.Println("The App is running in development env")
+	if err := viper.ReadInConfig(); err != nil {
+		log.Println("No local .env file found, relying on environment variables")
 	}
-	// Importantly we want to return a pointer to the created enviromnet variable. Although it was created locally the go 
-	// compiler is smart enough to recongnize that since we are returning the variable we want to continue to use the variable 
-	// after the function has been disposed. This means that the function moves from the stack to the heap. 
+
+	// Map environment variables to struct
+	if err := viper.Unmarshal(&env); err != nil {
+		log.Fatalf("Failed to unmarshal environment variables: %v", err)
+	}
+
+	// Log if in development
+	if env.AppEnv == "development" {
+		log.Println("The App is running in development environment")
+	}
+
 	return &env
+
 }
